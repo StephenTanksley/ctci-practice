afaee3c6d351de5e426fcc9147eae66f0315d873
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-10-03 16:29:21 -0500

    Added another test for spaces.
    Added a set implementation of the function.

--- Ch1_Arrays/isUnique.py
+++ Ch1_Arrays/isUnique.py
@@ -3,20 +3,35 @@
     
     Naive solution: Implement a hashmap as a counter. Loop over the string to add elements to a hashmap, then run a loop to check characters against the keys in the hashmap. If a character is already in the hashmap, return false (not all of the characters in the string are unique). Otherwise, return true.
 
+    Slightly less naive solution: We don't even need the hashmap, really. All we need is the key (the character itself) in a set. We can add all of our items in the 
+
 
 """
 
 
+# def is_unique(string):
+#     table = {}
+
+#     for char in string:
+#         if char not in table:
+#             table[char] = 1
+#         else:
+#             table[char] += 1
+
+#     for char in string:
+#         if table[char] > 1:
+#             return False
+#     return True
+
 def is_unique(string):
-    table = {}
+    str_set = set()
 
     for char in string:
-        if char not in table:
-            table[char] = 1
+        if char not in str_set:
+            str_set.add(char)
         else:
-            table[char] += 1
-
-    for char in string:
-        if table[char] > 1:
             return False
     return True
+
+
+print(is_unique("abcdee"))
--- Ch1_Arrays/test_isUnique.py
+++ Ch1_Arrays/test_isUnique.py
@@ -5,12 +5,15 @@ from isUnique import is_unique
 class TestIsUnique(unittest.TestCase):
     def test_true(self):
         str_true = "abcde"
-
+        str_space = " "
         self.assertIs(is_unique(str_true), True)
+        self.assertIs(is_unique(str_space), True)
 
     def test_false(self):
         str_false = "abcded"
+        str_space = "  "
         self.assertIs(is_unique(str_false), False)
+        self.assertIs(is_unique(str_space), False)
 
 
 if __name__ == '__main__':
