30d7a0ce5c7e39f33a0c9c333f21c8e056bd23f1
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-10-04 22:57:09 -0500

    feat: updated comments.

--- Ch1_Arrays/1.2/isPermutation.py
+++ Ch1_Arrays/1.2/isPermutation.py
@@ -7,25 +7,29 @@
 
     A naive approach might be to use one string to populate a set. Then, using the second string, check whether each character in that string exists in the set.
 
-    This approach would be O(N) time complexity with O(N) space complexity. The set would take up N amount of space. 
+    This approach would be O(N) time complexity with O(N) space complexity. 
+    
+    The set would take up N amount of space where N is the length of the string. This approach assumes that the strings are of equal length.
 
     THINGS TO CONSIDER:
 
     1) What if the strings are not the same length?
         1a) They can't be an exact permutation if they're different lengths.
 
-    2) What if there are non-character punctuation marks?
-        2a) That would remove 
+    2) What if both strings are the same?
+        2a) That means it wouldn't be a permutation.
 
 """
 
 
 def is_permutation(string_1, string_2):
 
+    # Edge case #1 - If the strings provided are the same.
     if string_1 == string_2:
         print("This is the same string, not a permutation.")
         return False
 
+    # Edge case #2 - If the strings are different lengths.
     if len(string_1) != len(string_2):
         return False
 
