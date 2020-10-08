661e11115221139dd8c9e3e4e285d4e9412e8f98
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-10-07 22:57:18 -0500

    misc: Writing pseudocode and strategizing to solve problem.

--- Ch1_Arrays/1.5/one_away.py
+++ Ch1_Arrays/1.5/one_away.py
@@ -1,6 +1,17 @@
 """
     PROBLEM: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
 
+    SOLUTION: Off the top of my head, I want to find a way of comparing the two strings one to one. To that end, I could initialize a loop to run through both strings and compare each character as I come into contact with it.
+
+        1) If the characters match, we move on to the next set of characters.
+        2) If the characters don't match, we can increment a counter.
+        3) If the counter == 1 at the end of the string, we can return True.
+        4) If the counter is > 1, we have more than one required edit and should return False.
+        
+        SUB PROBLEMS: 
+            1) What if the strings are the same? We can check this at the beginning. if str_1 == str_2, return False.
+            2) What if there are 
+
 """
 
 
